0c28236471a5c2d3d56bb7b668873828f7d8ecd1
Ruben Santana <rubensantana@outlook.com>
2024-11-16 20:16:06 -0400

    new article

--- src/app/page.js
+++ src/app/page.js
@@ -1,6 +1,6 @@
 import { allBlogs } from "contentlayer/generated";
 import HomeCoverSection from "../components/Home/HomeCoverSection";
-// import FeaturedPosts from "../components/Home/FeaturedPosts";
+import FeaturedPosts from "../components/Home/FeaturedPosts";
 // import RecentPosts from "../components/Home/RecentPosts";
 
 export default function Home() {
@@ -8,8 +8,8 @@ export default function Home() {
     return (
         <main className="flex flex-col items-center justify-between">
             <HomeCoverSection blogs={allBlogs} />
-            {/* <FeaturedPosts blogs={allBlogs} />
-            <RecentPosts blogs={allBlogs} /> */}
+            <FeaturedPosts blogs={allBlogs} />
+            {/* <RecentPosts blogs={allBlogs} /> */}
         </main>
     );
 }
--- src/components/Home/HomeCoverSection.js
+++ src/components/Home/HomeCoverSection.js
@@ -7,7 +7,7 @@ import { slug } from "github-slugger";
 
 const HomeCoverSection = ({ blogs }) => {
     const sortedBlogs = sortBlogs(blogs);
-    const blog = sortedBlogs[0];
+    const blog = sortedBlogs[1];
     return (
         <div className="inline-block w-full">
             <article className="flex flex-col items-start justify-end mx-5 sm:mx-10 relative h-[60vh] sm:h-[85vh]">
